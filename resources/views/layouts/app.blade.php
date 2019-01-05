599192b1ab10dd32bfff7a57cfdaa52127dd74b5
sameh <sameh.taha@ilerra.com>
2019-01-05 18:18:12 +0200

    Add theme update #7

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,36 +1,3 @@
-<head>
-  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-  <!-- Meta, title, CSS, favicons, etc. -->
-  <meta charset="utf-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-
-  <title>{{ config('app.name', 'Laravel') }}</title>
-
-  <!-- Bootstrap -->
-  <link href="assist/vendors/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
-  <!-- Font Awesome -->
-  <link href="assist/vendors/font-awesome/css/font-awesome.min.css" rel="stylesheet">
-  <!-- NProgress -->
-  <link href="assist/vendors/nprogress/nprogress.css" rel="stylesheet">
-  <!-- iCheck -->
-  <link href="assist/vendors/iCheck/skins/flat/green.css" rel="stylesheet">
-
-  <!-- bootstrap-progressbar -->
-  <link href="assist/vendors/bootstrap-progressbar/css/bootstrap-progressbar-3.3.4.min.css" rel="stylesheet">
-  <!-- JQVMap -->
-  <link href="assist/vendors/jqvmap/dist/jqvmap.min.css" rel="stylesheet"/>
-  <!-- bootstrap-daterangepicker -->
-  <link href="assist/vendors/bootstrap-daterangepicker/daterangepicker.css" rel="stylesheet">
-
-  <!-- Custom Theme Style -->
-  <link href="assist/build/css/custom.min.css" rel="stylesheet">
-</head>
-
-
 <!DOCTYPE html>
 <html lang="en">
   <head>
